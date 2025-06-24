c49ac2281f3b28417f1ee00bb255be47c793d367
JDragon <lee411806@naver.com>
2025-06-24 13:12:37 +0900

    init commit

---
+++ src/main/java/com/jaeyong/settlementstatisticsmsa/SettlementStatisticsMsaApplication.java
@@ -0,0 +1,13 @@
+package com.jaeyong.settlementstatisticsmsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettlementStatisticsMsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SettlementStatisticsMsaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jaeyong/settlementstatisticsmsa/SettlementStatisticsMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaeyong.settlementstatisticsmsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettlementStatisticsMsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
