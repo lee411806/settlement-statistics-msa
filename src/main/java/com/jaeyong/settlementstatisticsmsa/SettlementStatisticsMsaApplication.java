0082b2d73ebdca952d47fed5fcae4549251a8804
JDragon <lee411806@naver.com>
2025-06-24 16:30:35 +0900

    feat: config server, eureka server 연동
    - eureka server,mysql connector 의존성 추가
    - config server 연동 환경설정 추가

--- src/main/java/com/jaeyong/settlementstatisticsmsa/SettlementStatisticsMsaApplication.java
+++ src/main/java/com/jaeyong/settlementstatisticsmsa/SettlementStatisticsMsaApplication.java
@@ -2,8 +2,10 @@ package com.jaeyong.settlementstatisticsmsa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SettlementStatisticsMsaApplication {
 
     public static void main(String[] args) {
